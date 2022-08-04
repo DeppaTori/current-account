ba66687af0e3b61dcb8588a8259e7c47ae2e1bea
Tommy Toban <tommy.deppatori@gmail.com>
2022-08-04 10:33:09 +0700

    add popup confirmation

--- src/pages/PendingTask.js
+++ src/pages/PendingTask.js
@@ -1,9 +1,86 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
 export const PendingTask = () => {
+  const [open, setOpen] = useState(false);
+  const [openReject, setOpenReject] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleClickOpenReject = () => {
+    setOpenReject(true);
+  };
+
+  const handleCloseReject = () => {
+    setOpenReject(false);
+  };
+
+  const DialogPopup = () => {
+    return (
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Confirmation</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Anda yakin menyetujui task ini?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Batal</Button>
+          <Button onClick={handleClose} autoFocus>
+            Ya
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+
+  const DialogPopupReject = () => {
+    return (
+      <Dialog
+        open={openReject}
+        onClose={handleCloseReject}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Confirmation</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Anda yakin menolak task ini?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseReject}>Batal</Button>
+          <Button onClick={handleCloseReject} autoFocus>
+            Ya
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+
   return (
     <>
-      <button>Approve</button>
+      <button onClick={handleClickOpen}>Approve</button>
       <br />
-      <button>Reject</button>
+      <button onClick={handleClickOpenReject}>Reject</button>
+      <DialogPopup />
+      <DialogPopupReject />
     </>
   );
 };
--- src/pages/__tests__/PendingTask.test.js
+++ src/pages/__tests__/PendingTask.test.js
@@ -1,3 +1,4 @@
+import user from "@testing-library/user-event";
 const { render, screen } = require("@testing-library/react");
 const { PendingTask } = require("../PendingTask");
 
@@ -6,5 +7,21 @@ describe("PendingTask", () => {
     render(<PendingTask />);
     expect(screen.getByRole("button", { name: "Approve" })).toBeInTheDocument();
     expect(screen.getByRole("button", { name: "Reject" })).toBeInTheDocument();
+  });
+
+  it("renders popup confirmation when approve button is clicked", async () => {
+    render(<PendingTask />);
+    await user.click(screen.getByRole("button", { name: "Approve" }));
+    expect(
+      screen.getByText("Anda yakin menyetujui task ini?")
+    ).toBeInTheDocument();
+  });
+
+  it("renders popup reject when reject button is clicked", async () => {
+    render(<PendingTask />);
+    await user.click(screen.getByRole("button", { name: "Reject" }));
+    expect(
+      screen.getByText("Anda yakin menolak task ini?")
+    ).toBeInTheDocument();
   });
 });
